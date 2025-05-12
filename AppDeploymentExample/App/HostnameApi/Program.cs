c6388cbea8a0055eea8a4c48d2838ab077fd141a
bulat <farukshin@molot.ru>
2025-05-12 17:17:46 +0300

    add another app - fix

---
+++ AppDeploymentExample/App/HostnameApi/Program.cs
@@ -0,0 +1,54 @@
+using System.Net;
+
+namespace HostnameApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var hostname = Dns.GetHostName();
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return new WeatherForecastResponse(hostname, forecast);
+        })
+        .WithName("GetWeatherForecast");
+
+        app.Run();
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+record WeatherForecastResponse(string Hostname, WeatherForecast[] Forecast);
